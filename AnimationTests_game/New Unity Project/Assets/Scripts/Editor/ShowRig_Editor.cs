0f70cc9c025ee496fe6c5c87d0ad75fe6677e466
DomCristaldi <domeniccristaldi@gmail.com>
2015-12-17 03:07:40 -0500

    ++ Made a script to show the rig visually ++ Updated rig, made it better ++ started the animation workflow on the rig

---
+++ AnimationTests_game/New Unity Project/Assets/Scripts/Editor/ShowRig_Editor.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(ShowRig))]
+public class ShowRig_Editor : Editor {
+
+
+
+    //ShowRig selfScript;
+
+    void OnEnable() {
+        //selfScript = (ShowRig)target;
+
+        //SceneView.onSceneGUIDelegate += DrawRigFunction;
+    }
+
+    //void OnSceneGUI() {
+    [DrawGizmo(GizmoType.NotInSelectionHierarchy | GizmoType.InSelectionHierarchy)]
+    static void DrawRigFunction(Transform objTransform, GizmoType gizmoType) {
+
+        //ShowRig selfScript = (ShowRig)target;
+
+
+        Color rigColor = Color.blue;
+        float rigSphereSize = 0.07f;
+
+        Color originalColor = Handles.color;
+        Handles.color = rigColor;
+
+        foreach (Transform tf in objTransform.GetComponentsInChildren<Transform>()) {
+
+            if (tf.tag == "RigJoint") {
+
+            Handles.SphereCap(0,
+                               tf.position,
+                               Quaternion.identity,
+                               rigSphereSize);
+
+                if (tf.parent != null && tf.parent.tag == "RigJoint") {
+
+                    Handles.DrawLine(tf.parent.position, tf.position);
+
+                    /*Handles.CylinderCap(0,
+                                        Vector3.Lerp(tf.position, tf.parent.position, 0.5f),
+                                        Quaternion.LookRotation(tf.parent.position - tf.position),
+                                        0.05f);
+                    */
+                }
+            }
+
+
+            
+
+            
+        }
+
+        /*
+        Handles.SphereCap(0,
+                          selfScript.transform.position,
+                          Quaternion.identity,
+                          selfScript.sphereSize);
+        */
+
+
+        Handles.color = originalColor;
+    }
+    
+}
---
+++ AnimationTests_game/New Unity Project/Assets/Scripts/ShowRig.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowRig : MonoBehaviour {
+
+    public Color rigColor = Color.blue;
+
+    public float sphereSize = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    /*
+    void OnDrawGizmos() {
+        foreach (Transform tf in transform) {
+
+        }
+    }
+    */
+}
