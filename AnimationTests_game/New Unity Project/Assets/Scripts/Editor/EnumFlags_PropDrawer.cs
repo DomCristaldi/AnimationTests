06ac090de0587165862eadf895c6184931d55f37
DomCristaldi <domeniccristaldi@gmail.com>
2015-12-19 11:01:38 -0500

    .. Started rebuilding animations .. Fixed scaling on RigJoints of Character's left arm

---
+++ AnimationTests_game/New Unity Project/Assets/Scripts/Editor/EnumFlags_PropDrawer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+
+[CustomPropertyDrawer(typeof(EnumFlagsAttribute))]
+public class EnumFlags_PropDrawer : PropertyDrawer {
+
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+        //base.OnGUI(position, property, label);
+
+        property.intValue = EditorGUI.MaskField(position, label, property.intValue, property.enumNames);
+    }
+
+}
--- AnimationTests_game/New Unity Project/Assets/Scripts/ShowRig.cs
+++ AnimationTests_game/New Unity Project/Assets/Scripts/ShowRig.cs
@@ -1,11 +1,42 @@
 ﻿using UnityEngine;
-using System.Collections;
+//using System;
+using System.Collections.Generic;
 
 public class ShowRig : MonoBehaviour {
+
+    [System.Serializable]
+    public class MirrorBinding {
+
+        [System.Flags]
+        public enum MirrorSetting {
+            xPosition = 0x1,
+            yPosition = 0x2,
+            zPosition = 0x4,
+
+            xRotation = 0x8,
+            yRotation = 0x16,
+            zRotation = 0x32,
+
+            xScale = 0x64,
+            yScale = 0x128,
+            zScale = 0x256,
+        }
+
+        [SerializeField]
+        [EnumFlags]
+        public MirrorSetting setting;
+
+        public GameObject[] mirroredObjects;
+
+
+
+    }
 
     public Color rigColor = Color.blue;
 
     public float sphereSize = 0.2f;
+
+    public List<MirrorBinding> mirrorList;
 
 	// Use this for initialization
 	void Start () {
